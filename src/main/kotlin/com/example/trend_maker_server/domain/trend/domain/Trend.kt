5df4b811499cfe156f2b8bd71960c6ee936688d3
101 <tmddnjs001133@dsm.hs.kr>
2024-02-08 11:05:19 +0900

    🌧 :: Trend model 추가
    Database : MongoDB, Collection : Trend

---
+++ src/main/kotlin/com/example/trend_maker_server/domain/trend/domain/Trend.kt
@@ -0,0 +1,25 @@
+package com.example.trend_maker_server.domain.trend.domain
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+
+@Document(collection = "trend")
+class Trend(
+    @Id
+    val id: ObjectId = ObjectId.get(),
+
+    var content: String,
+
+    val user: String, // User로 이후에 변경
+
+    val category: String,
+
+    val createdDate: LocalDateTime,
+    var updatedDate: LocalDateTime, // 로컬 말고 한국 시간으로 저장해야함.
+
+    var likes: List<String> = arrayListOf(),
+    var views: Int = 0
+) {
+}
